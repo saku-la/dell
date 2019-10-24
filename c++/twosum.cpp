87336382ab6da360285720ad0b91e552b371ade5
syjjsy <2015710206@smail.xtu.edu.cn>
2019-10-24 20:50:36 +0800

    Update twosum.cpp

--- c++/twosum.cpp
+++ c++/twosum.cpp
@@ -1,3 +1,6 @@
+//定义一个数组和一个目标，判断数组里
+//是否有两个数能组成目标
+
 #include "stdafx.h"
 #include<iostream>
 #include<vector>
@@ -27,23 +30,7 @@ public:
 	}
 };
 
-//vector<int> twoSum(vector<int> nums, int target)
-//{
-//	map<int, int> m;
-//	vector<int> vec;
-//	for (int i = 0; i < nums.size(); i++)
-//	{
-//		int b = target - nums[i];
-//		if (m.count(b))
-//		{
-//			vec.push_back(m[b]);
-//			vec.push_back(i);
-//			break;
-//		}
-//		m[nums[i]] = i;
-//	}
-//	return vec;
-//}
+
 
 void main()
 {
@@ -51,7 +38,7 @@ void main()
 	int target = 7;
 	solution solution1;
 	vector<int> a = solution1.twoSum(nums, target);
-	//vector<int> a = twoSum(nums, target);
+	
 	for (int i = 0; i<a.size(); i++)
 		std::cout << a[i] << endl;
 	system("Pause");
