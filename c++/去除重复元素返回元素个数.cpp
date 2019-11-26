a0967c6924a4595b2b7af4705cf6ca10b755670c
syjjsy <2015710206@smail.xtu.edu.cn>
2019-11-26 20:28:31 +0800

    Update 去除重复元素返回元素个数.cpp

--- c++/去除重复元素返回元素个数.cpp
+++ c++/去除重复元素返回元素个数.cpp
@@ -10,15 +10,15 @@ using namespace std;
 class Sloution {
 public:
 	int remove(vector<int>& nums) {
-		auto iter = unique(nums.begin(), nums.end());
-		nums.erase(iter, nums.end());
+		auto iter = unique(nums.begin(), nums.end());//将相邻重复元素放在最后
+		nums.erase(iter, nums.end());//删除最后的元素
 		return nums.size();
 	}
 };
 int main() {
 	vector<int> a = { 1,3,2,4,6,5,7,3,4,5,3,2,5,3 };
-	sort(a.begin(), a.end());
-	Sloution a1;
+	sort(a.begin(), a.end());//将数组排序
+	Sloution a1;//实例
 	int c=a1.remove(a);
 	cout << c << endl;
 	for (int i = 0; i < a.size(); i++)
